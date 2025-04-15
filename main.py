4598b12a528e42e0ec85710a7f3248e1242ce0b1
Nour Hamdan <nour.ham96@gmail.com>
2025-04-15 22:14:18 +0300

    Adding more url endpoints to our api

--- main.py
+++ main.py
@@ -22,6 +22,28 @@ def about(station, date):
         "date": date,
         "temperature": temperature
     }
+    
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    # orient = records:: Give me a list of dictionaries, where each row in the dataframe
+    # becomes one dictionary (a record)
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    # we removed parse_date cuz we want python to read date as an integer, not as a date, then we can
+    # convert it into string
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    
+    return result
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
